0abe1bfbae61bc95c7efa51c252ee673be981106
Evgeniy <vovasevgeniy@gmail.com>
2021-10-04 18:23:29 +0300

    Добавляет первые React-компоненты

---
+++ project/src/components/main-page/main-page.tsx
@@ -0,0 +1,76 @@
+import Header from '../header/header';
+import PlacesSorting from '../places-sorting/places-sorting';
+import PlaceCard from '../place-card/place-card';
+
+type MainPageProps = {
+  placesCount: number;
+}
+
+function MainPage({placesCount}: MainPageProps): JSX.Element {
+  return (
+    <div className='page page--gray page--main'>
+      <Header/>
+      <main className='page__main page__main--index'>
+        <h1 className='visually-hidden'>Cities</h1>
+        <div className='tabs'>
+          <section className='locations container'>
+            <ul className='locations__list tabs__list'>
+              <li className='locations__item'>
+                <a className='locations__item-link tabs__item' href='#'>
+                  <span>Paris</span>
+                </a>
+              </li>
+              <li className='locations__item'>
+                <a className='locations__item-link tabs__item' href='#'>
+                  <span>Cologne</span>
+                </a>
+              </li>
+              <li className='locations__item'>
+                <a className='locations__item-link tabs__item' href='#'>
+                  <span>Brussels</span>
+                </a>
+              </li>
+              <li className='locations__item'>
+                <a className='locations__item-link tabs__item tabs__item--active'>
+                  <span>Amsterdam</span>
+                </a>
+              </li>
+              <li className='locations__item'>
+                <a className='locations__item-link tabs__item' href='#'>
+                  <span>Hamburg</span>
+                </a>
+              </li>
+              <li className='locations__item'>
+                <a className='locations__item-link tabs__item' href='#'>
+                  <span>Dusseldorf</span>
+                </a>
+              </li>
+            </ul>
+          </section>
+        </div>
+        <div className='cities'>
+          <div className='cities__places-container container'>
+            <section className='cities__places places'>
+              <h2 className='visually-hidden'>Places</h2>
+              <b className='places__found'>{placesCount} places to stay in Amsterdam</b>
+              <PlacesSorting/>
+              <div className='cities__places-list places__list tabs__content'>
+                <PlaceCard/>
+                <PlaceCard/>
+                <PlaceCard/>
+                <PlaceCard/>
+                <PlaceCard/>
+
+              </div>
+            </section>
+            <div className='cities__right-section'>
+              <section className='cities__map map'/>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default MainPage;
