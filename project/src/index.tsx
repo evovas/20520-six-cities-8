8d2464f7f857c6b9942d66cff1b3fade1ef95078
Evgeniy <vovasevgeniy@gmail.com>
2021-11-14 23:34:01 +0300

    Подключает Redux Toolkit. WIP

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,27 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {applyMiddleware, createStore} from '@reduxjs/toolkit';
-import thunk from 'redux-thunk';
+import {configureStore} from '@reduxjs/toolkit';
 import {Provider} from 'react-redux';
-import {composeWithDevTools} from 'redux-devtools-extension';
 import {ToastContainer} from 'react-toastify';
 import App from './components/app/app';
 import {ThunkAppDispatch} from './types/action';
-import {reducer} from './store/reducer';
 import {checkAuthSuccess} from './store/action';
 import {checkAuthAction, fetchOffersAction} from './store/api-actions';
 import {createAPI} from './services/api';
 import {AuthorizationStatus} from './const';
 import 'react-toastify/dist/ReactToastify.css';
+import {rootReducer} from './store/root-reducer';
 
 const api = createAPI(
   () => store.dispatch(checkAuthSuccess(AuthorizationStatus.NoAuth)),
 );
 
-const store = createStore(
-  reducer,
-  composeWithDevTools(applyMiddleware(thunk.withExtraArgument(api))),
-);
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      thunk: {
+        extraArgument: api,
+      },
+    }),
+});
 
 (store.dispatch as ThunkAppDispatch)(checkAuthAction());
 (store.dispatch as ThunkAppDispatch)(fetchOffersAction());
